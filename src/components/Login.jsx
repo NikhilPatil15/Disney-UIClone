bf93e0e00e269ab8ee2311dca37233e4bf053085
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-03-03 14:23:58 +0530

    Login Component Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <Content>
+        <CTA>
+          <LogoOne src="../public/images/cta-logo-one.svg" alt="First-Logo" />
+          <Button>Get it all here</Button>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <LogoTwo src='../public/images/cta-logo-two.png'/>
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
 
-export default Login
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  margin-bottom: 10vw;
+  height: 100%;
+  padding: 80px 40px;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
+  background-image: url("../public/images/login-background.jpg");
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 800px;
+  width: 100%;
+  gap: 1rem;
+`;
+
+const LogoOne = styled.img`
+  display: block;
+  width: 100%;
+  max-width: 800px;
+  min-height: 1px;
+  margin-bottom: 12px;
+`;
+
+const Button = styled.a`
+  background-color: #0063e5;
+  text-align: center;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  position: relative;
+  letter-spacing: 1.5px;
+  border-radius: 0.25rem;
+  font-weight: bold;
+  margin-bottom: 12px;
+  padding: 10px;
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
+
+const Description = styled.p`
+font-size: 18px;
+letter-spacing: 1.5px;
+text-align: center;
+`
+
+const LogoTwo = styled.img`
+  max-width: 800px;
+  width: 100%;
+  min-height: 1px;
+  margin-bottom: 12px;
+`
+export default Login;
