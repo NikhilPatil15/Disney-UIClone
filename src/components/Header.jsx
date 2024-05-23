31acd90a9d6eb873fbb04647b9fdec0d8fa88cf5
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-05-24 03:39:09 +0530

    signin changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { auth, provider } from "../firebase";
-import { signInWithPopup, signInWithRedirect, signOut } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setSignOut, setUserLogin } from "../redux/features/user";
 
 const Header = () => {
+  const myref = useRef(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userName = useSelector((state) => state.user.name);
+  const email = useSelector((state) => state.user.email);
   const photo = useSelector((state) => state.user.photo);
 
   const setUser = (user) => {
@@ -61,42 +63,45 @@ const Header = () => {
         navigate("/home");
       }
     });
-  }, [userName, navigate]);
-
+  }, [userName]);
   return (
     <NavBar>
       <LogoArea>
         <Link to="/">
-          <Logo src="../images/logo.svg" alt="Logo" />
+          <Logo src="../images/logo.svg" />
         </Link>
       </LogoArea>
+      {/* <NavItems className="drop">
+        <img src="../images/dropdown.png"/>
+       </NavItems> */}
+      
       {!userName ? (
         <LoginButton onClick={handleAuth}>Login</LoginButton>
       ) : (
         <>
           <NavItems>
-            <Link to="/home">
+            <Link to={"/home"}>
               <img src="../images/home-icon.svg" alt="Home" />
               <span>HOME</span>
             </Link>
-            <Link to="#">
-              <img src="../images/search-icon.svg" alt="Search" />
+            <Link>
+              <img src="../images/search-icon.svg" alt="Home" />
               <span>SEARCH</span>
             </Link>
-            <Link to="#">
-              <img src="../images/watchlist-icon.svg" alt="Watchlist" />
+            <Link>
+              <img src="../images/watchlist-icon.svg" alt="Home" />
               <span>WATCHLIST</span>
             </Link>
-            <Link to="#">
-              <img src="../images/original-icon.svg" alt="Originals" />
-              <span>ORIGINALS</span>
+            <Link>
+              <img src="../images/original-icon.svg" alt="Home" />
+              <span>ORIGNALS</span>
             </Link>
-            <Link to="#">
-              <img src="../images/movie-icon.svg" alt="Movies" />
+            <Link>
+              <img src="../images/movie-icon.svg" alt="Home" />
               <span>MOVIES</span>
             </Link>
-            <Link to="#">
-              <img src="../images/series-icon.svg" alt="Series" />
+            <Link>
+              <img src="../images/series-icon.svg" alt="Home" />
               <span>SERIES</span>
             </Link>
           </NavItems>
@@ -114,80 +119,124 @@ const Header = () => {
 
 const NavBar = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
   right: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
   height: 60px;
+  max-height: 80px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   z-index: 3;
   background-color: #040714;
-  padding: 0 20px;
+  width: 100%;
+  padding: 2rem;
 
-  @media (max-width: 768px) {
-    padding: 0 10px;
-  }
+  /* .drop{
+    display: none;
+  } */
+
+  /* @media (max-width:768px) {
+    .drop{
+      display:block;
+      width: fit-content;
+      background-color: white;
+    } */
+  
+
 `;
 
 const LogoArea = styled(Link)`
   display: inline-block;
   width: 100px;
-  height: 60px;
+  max-height: 80px;
 `;
 
+
+
 const Logo = styled.img`
+  display: block;
   width: 100%;
-  height: 100%;
-  object-fit: cover;
+  max-width: 150px;
+  height: 60px;
+  max-height: 100px;
+  margin: 0;
 `;
 
 const NavItems = styled.nav`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  flex-grow: 1;
+  position: relative;
+  flex-flow: row nowrap;
+  margin-right: auto;
+  margin-left: 16px;
   gap: 1rem;
 
   a {
     display: flex;
+    justify-content: center;
     align-items: center;
-    gap: 0.2rem;
+    flex-direction: row;
+    height: 100%;
     padding: 0 12px;
-    text-decoration: none;
-    color: white;
+    gap: 0.2rem;
 
     img {
-      height: 20px;
-      width: 20px;
+      height: 30px;
+      width: 40px;
+      max-width: 40px;
+      z-index: auto;
     }
 
     span {
-      letter-spacing: 1px;
-      font-size: 14px;
-    }
+      position: relative;
+      top:3px;
+      letter-spacing: 3px;
+      line-height: 1.4;
+      white-space: nowrap;
+      font-size: 18px;
 
-    &:hover span::before {
-      visibility: visible;
-      transform: scaleX(1);
-      opacity: 1;
+      &:before {
+        content: "";
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        opacity: 0;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        transform-origin: center;
+        position: absolute;
+        bottom: -6px;
+        right: 0;
+        left: 0;
+        height: 2px;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        visibility: visible;
+        transform: scaleX(1);
+        opacity: 1 !important;
+      }
     }
   }
-
-  @media (max-width: 768px) {
+  @media (max-width:1200px){
     display: none;
+
   }
 `;
 
-const LoginButton = styled.button`
+const LoginButton = styled(Link)`
   border: 2px solid #fff;
   box-shadow: 0px 4px 8px rgb(249, 249, 249 / 16%);
   background-color: #0d0d2b;
   padding: 0.5rem 1rem;
   border-radius: 0.5rem;
   transition: all 0.2s ease-in-out;
-  color: white;
-  cursor: pointer;
+  padding: 8px 16px;
 
   &:hover {
     background-color: #f9f9f9;
@@ -196,8 +245,9 @@ const LoginButton = styled.button`
 `;
 
 const UserImgIcon = styled.img`
-  height: 100%;
+height: 100%;
 `;
+
 
 const Dropdown = styled.div`
   position: absolute;
@@ -206,7 +256,7 @@ const Dropdown = styled.div`
   width: 100px;
   background-color: #212121;
   border: 1px solid gray;
-  box-shadow: rgb(0, 0, 0 / 50%) 0px 0px 10px 0px;
+  box-shadow: rgb(0, 0, 0/50%) 0px 0px 10px 0px;
   font-size: 1.2rem;
   letter-spacing: 1px;
   border-radius: 0.4rem;
@@ -223,20 +273,23 @@ const Signout = styled.div`
   height: 40px;
   width: 40px;
   cursor: pointer;
+  
 
-  ${UserImgIcon} {
-    border-radius: 50%;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
+  ${UserImgIcon}{
+      border-radius: 50%;
+      object-fit:cover;
+      width: 100%;
+      height: 100%;
   }
 
-  &:hover {
-    ${Dropdown} {
+  &:hover{
+    ${Dropdown}{
       opacity: 1;
       transition: all 0.2s ease;
     }
   }
 `;
+
+
 
 export default Header;
